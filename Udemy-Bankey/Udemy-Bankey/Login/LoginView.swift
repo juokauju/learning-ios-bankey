e3ea82d658a7381cc4a37b1e7b3938814691534f
Justina Siaulyte <justina.siautyte@protonmail.com>
2023-01-17 11:51:54 +0200

    Finish `LoginView` and add sighInButton to `LoginViewController`

--- Udemy-Bankey/Udemy-Bankey/Login/LoginView.swift
+++ Udemy-Bankey/Udemy-Bankey/Login/LoginView.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class LoginView: UIView {
     
+    private let stackView = UIStackView()
+    private let usernameTextField = UITextField()
+    private let passwordTextField = UITextField()
+    private let dividerView = UIView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
@@ -18,19 +23,82 @@ class LoginView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
-    }
 }
 
+// MARK: - Style and Layout
 extension LoginView {
     private func style() {
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .orange
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 5
+        clipsToBounds = true
+        
+        setStackView()
+        setUsernameTextField()
+        setPasswordTextField()
+        setDividerView()
     }
     
     private func layout() {
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
+        addSubview(stackView)
         
+        addStackViewConstraints()
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    private func setStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+    }
+    
+    private func setUsernameTextField() {
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
+    }
+    
+    private func setPasswordTextField() {
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.delegate = self
+    }
+    
+    private func setDividerView() {
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .secondarySystemFill
+    }
+    
+    private func addStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
+        ])
+    }
+}
+
+// MARK: - UITextFiueldDelegate
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
     }
 }
--- Udemy-Bankey/Udemy-Bankey/Login/LoginViewController.swift
+++ Udemy-Bankey/Udemy-Bankey/Login/LoginViewController.swift
@@ -10,29 +10,54 @@ import UIKit
 class LoginViewController: UIViewController {
     
     private let loginView = LoginView()
+    private let sighInButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
-
-
 }
 
 extension LoginViewController {
     private func style() {
-        
+        setSighInButton()
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(sighInButton)
         
+        addLoginViewConstraints()
+        addSighInButtonConstraints()
+    }
+    
+    private func addLoginViewConstraints() {
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
+    }
+    
+    private func addSighInButtonConstraints() {
+        NSLayoutConstraint.activate([
+            sighInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            sighInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            sighInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+    }
+    
+    private func setSighInButton() {
+        sighInButton.translatesAutoresizingMaskIntoConstraints = false
+        sighInButton.configuration = .filled()
+        sighInButton.configuration?.imagePadding = 8
+        sighInButton.setTitle("Sigh In", for: [])
+        sighInButton.addTarget(self, action: #selector(sighInTapped), for: .primaryActionTriggered)
+    }
+    
+    @objc private func sighInTapped(sender: UIButton) {
+        
     }
 }
 
