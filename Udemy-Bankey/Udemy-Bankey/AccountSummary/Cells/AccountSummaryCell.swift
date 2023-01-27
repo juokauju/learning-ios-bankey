e418565afedaaa62d8b879f1808cbd1ca51f2a61
Justina Siaulyte <justina.siautyte@protonmail.com>
2023-01-27 16:59:38 +0200

    feat: Add view model of account summary cell

--- Udemy-Bankey/Udemy-Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ Udemy-Bankey/Udemy-Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class AccountSummaryCell: UITableViewCell {
     
+    enum AccountType: String {
+        case Banking
+        case CreditCard
+        case Investment
+    }
+    
+    struct ViewModel {
+        let accountType: AccountType
+        let accountName: String
+    }
+    
     static let reuseID = "AccountSummaryCell"
     static let rowHeight: CGFloat = 112
     
@@ -141,4 +152,23 @@ extension AccountSummaryCell {
             
             return rootString
         }
+}
+
+extension AccountSummaryCell {
+    func configure(with vm: ViewModel) {
+        typeLabel.text = vm.accountType.rawValue
+        nameLabel.text = vm.accountName
+        
+        switch vm.accountType {
+        case .Banking:
+            underlineView.backgroundColor = appColor
+            balanceLabel.text = "Current balance"
+        case .CreditCard:
+            underlineView.backgroundColor = .systemOrange
+            balanceLabel.text = "Current balance"
+        case .Investment:
+            underlineView.backgroundColor = .systemPurple
+            balanceLabel.text = "Value"
+        }
+    }
 }
