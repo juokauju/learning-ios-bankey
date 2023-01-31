91178f188ff736d96eb03445c5c2e558a59e03c5
Justina Siaulyte <justina.siautyte@protonmail.com>
2023-01-31 13:42:56 +0200

    feat: Shake the bell

--- Udemy-Bankey/Udemy-Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Udemy-Bankey/Udemy-Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -11,6 +11,8 @@ class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
     
+    private let shakeyBellView = ShakeyBellView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -30,6 +32,7 @@ class AccountSummaryHeaderView: UIView {
         addSubview(contentView)
         contentView.backgroundColor = appColor
         addContentViewConstraints()
+        setupShakeyBell()
     }
     
     private func setBundle() {
@@ -43,6 +46,16 @@ class AccountSummaryHeaderView: UIView {
         contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    private func setupShakeyBell() {
+        shakeyBellView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shakeyBellView)
+        
+        NSLayoutConstraint.activate([
+            shakeyBellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shakeyBellView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
 
